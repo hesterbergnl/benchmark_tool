bc564185c5b528af79d38422386a957ae0177054
Nikolai Hesterberg <nikolai.hesterberg@gmail.com>
2019-12-31 21:17:19 -0500

    Implemented time measurement within code - uses clock cycles elapsed Implemented multithreading

--- benchmark.c
+++ benchmark.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <pthread.h>
+#include <time.h>
 
 int loopFib(int);
 int recFib(int);
@@ -10,6 +11,9 @@ int n, threads, val;
 
 int main()
 {
+	clock_t start, end;
+	double cpu_time_used;
+
 	printf("Please enter the number of fibbonaci sequence numbers> ");
 	scanf("%d", &n);
 	printf("Please enter the number of parallel threads> ");
@@ -21,6 +25,7 @@ int main()
 	pthread_attr_t attr;
 	pthread_attr_init(&attr);
 
+	start = clock();
 	for(int i = 0; i < threads; i++) {
 		pthread_create(&threadArray[i], &attr, recursiveThread, NULL);
 	}
@@ -28,11 +33,16 @@ int main()
 	for(int i = 0; i < threads; i++) {
 		pthread_join(threadArray[i], NULL);
 	}
+	end = clock();
+	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
     
-	printf("Without optimization, %d input size\n", n);
+	printf("Without optimization\n");
+	printf("Input size: %d\n", n);
+	printf("Final Value: %d\n", n);
 	printf("------------------------------------\n");
-	printf("Fibbonaci recursive algorithm: %d\n\n", val);
+	printf("Fibbonaci recursive algorithm, total time: %f seconds\n", cpu_time_used);
 	
+	start = clock();
 	for(int i = 0; i < threads; i++) {
 		pthread_create(&threadArray[i], &attr, recursiveThread, NULL);
 	}
@@ -40,9 +50,11 @@ int main()
 	for(int i = 0; i < threads; i++) {
 		pthread_join(threadArray[i], NULL);
 	}
+	end = clock();
+	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 	
 	
-	printf("Fibbonaci loop algoirthm: %d\n\n", val);	
+	printf("Fibbonaci loop algoirthm, total time: %f seconds\n", cpu_time_used);	
 	
 	return 0;
 }
